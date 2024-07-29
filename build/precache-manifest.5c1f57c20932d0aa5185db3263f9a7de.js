47d8af67cbc8e456c10d5af26a4d52a2c26b109d
Henry Lee <init.defaultBranch>
2024-07-29 11:25:06 -0400

    add title

---
+++ build/precache-manifest.5c1f57c20932d0aa5185db3263f9a7de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b7faf108ede78061880a77a92e6eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fce1bc651ca4fc0120e",
+    "url": "/static/css/main.73e20d8c.chunk.css"
+  },
+  {
+    "revision": "624a6851a5a56e196d04",
+    "url": "/static/js/2.3bd88014.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3bd88014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fce1bc651ca4fc0120e",
+    "url": "/static/js/main.826f7ce9.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
